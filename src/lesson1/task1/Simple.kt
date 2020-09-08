bd798a525ca357d9516544642535bba9d74b2c7c
tetrade <Dfyz7536320>
2020-09-08 11:53:12 +0300

    fixed 1 lesson accountInThreeYears

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -120,9 +120,10 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double =
-    initial * (1 + percent / 100.0) * (1 + percent / 100.0) * (1 + percent / 100.0)
-
+fun accountInThreeYears(initial: Int, percent: Int): Double {
+    val interest = (1 + percent / 100.0)
+    return initial * interest * interest * interest
+}
 /**
  * Простая (2 балла)
  *
