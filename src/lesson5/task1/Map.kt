ab874952ddc3971d5da44d08178b76e4d4d6348f
tetrade <piatlin2002@gmail.com>
2020-09-21 18:57:31 +0300

    fix

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -230,7 +230,8 @@ fun findCheapestStuff(stuff: Map<String, Pair<String, Double>>, kind: String): S
  * Например:
  *   canBuildFrom(listOf('a', 'b', 'o'), "baobab") -> true
  */
-fun canBuildFrom(chars: List<Char>, word: String): Boolean = chars.containsAll(word.toLowerCase().toSet())
+fun canBuildFrom(chars: List<Char>, word: String): Boolean =
+    chars.map{ it.toLowerCase() }.containsAll(word.toLowerCase().toSet())
 
 /**
  * Средняя (4 балла)
